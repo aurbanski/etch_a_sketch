cb300cccfca0b46cdee83de687a88453de1c2cc2
Alexander T. Urbanski <aurbanski27@gmail.com>
2016-10-30 18:13:49 -0600

    Started to set up user input

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+  $(".button").click(function(){
+    var px_size = prompt("What pixel size would you like?");
+    $(".grid_element").height(height + "px");
+    $(".grid_element").width(width + "px");
+  });
+
   for (var row = 0; row < 4; row++) {
-    $(".container").append('<div class="row' + row + '"></div>');
+    $(".container").append('<div class="row' + row + '" ></div>');
     for (var col = 0; col < 4; col++) {
       $(".row" + row).append('<div class="grid_element" class="grid' + col + '"></div>');
     }
